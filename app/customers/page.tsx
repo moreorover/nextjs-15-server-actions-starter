e78fa10483062e1f87c120ca7ade54522e80ad4b
Martin <mselvenis@gmail.com>
2024-11-30 16:57:27 +0000

    feat: seed customers

---
+++ app/customers/page.tsx
@@ -0,0 +1,29 @@
+import ClientComponent from "@/components/client-component";
+import { auth } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function CustomersPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    return redirect("/");
+  }
+
+  const customers = await prisma.customer.findMany();
+
+  return (
+    <div className="mt-10 text-center">
+      <h1 className="text-2xl font-bold underline">Welcome to the dashboard</h1>
+      <ul>
+        {customers.map((customer) => (
+          <li key={customer.id}>{customer.name}</li>
+        ))}
+      </ul>
+      <ClientComponent />
+    </div>
+  );
+}
